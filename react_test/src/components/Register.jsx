e33d2e9300ac2a47f54951943eab3b4effe8eacf
yashsakalley-1997 <sakalley.yash@gmail.com>
2022-04-17 21:58:37 +0530

    changes in register

--- react_test/src/components/Register.jsx
+++ react_test/src/components/Register.jsx
@@ -46,7 +46,7 @@ export const Register = ()=>{
 
                 <Form.Group className="mb-3" controlId="password" onChange={formChange}>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="text"/>
+                    <Form.Control type="password"/>
                 </Form.Group>
 
                 
